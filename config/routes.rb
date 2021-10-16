08eb6c9ee3c2ef35b2ac9e03160c8152e4c81457
Chukwuma Obasi <69759467+ccobasi@users.noreply.github.com>
2021-10-16 21:51:09 +0100

    add greeting router

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1, defaults: { format: :json } do
+      get 'messages', to: 'messages#index'
+      get 'random-greeting', to: 'messages#hello' 
+    end
+  end
+
+  get '*page', to: 'static#index', constraints: ->(req) do
+    !req.xhr? && req.format.html?
+  end
+
   root 'static#index'
 end
