82734900699f7f1552e46e9a46e6293b29e455b3
Chukwuma Obasi <69759467+ccobasi@users.noreply.github.com>
2021-10-24 21:10:11 +0100

    Update App.js

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import Greeting from './Greeting'
-import store from '../redux/store'
+import store from './redux/store'
 
 const App = () => (
   <>
